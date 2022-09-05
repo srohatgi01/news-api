372aaf1f68f26ddb9294ec014250019c372ce4cc
Sarthak Rohatgi <infinitybusiness726@gmail.com>
2022-09-05 14:36:46 +0530

    hero ani, article_detail, refresh indicator

--- lib/presentations/homepage.dart
+++ lib/presentations/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_api/data/models/latest_news.dart';
+import 'package:news_api/presentations/article_details.dart';
 
 import '../data/provider/api.dart';
 
@@ -13,9 +14,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var fetchdata = NewsApi().fetchLatestNews();
 
-  fetch() {
+  fetch() async {
+    print("Fetched called");
     setState(() {
       fetchdata = NewsApi().fetchLatestNews();
+      print("Data Fetched");
     });
   }
 
@@ -28,53 +31,75 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: const Text("News App"),
       ),
-      body: RefreshIndicator(
-        onRefresh: () => fetch(),
-        child: FutureBuilder(
-          future: fetchdata,
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done &&
-                snapshot.hasData) {
-              LatestNews news = snapshot.data as LatestNews;
-              return ListView.builder(
+      body: FutureBuilder(
+        future: fetchdata,
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            LatestNews news = snapshot.data as LatestNews;
+            return RefreshIndicator(
+              onRefresh: () => fetch(),
+              child: ListView.builder(
                 itemCount: news.articles.length,
                 itemBuilder: (context, index) => Card(
+                  elevation: 3,
                   margin: const EdgeInsets.all(16),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 15),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          news.articles[index].title!,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 22,
+                  child: GestureDetector(
+                    onTap: (() {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ArticleDetails(
+                            article: news.articles[index],
+                            articleIndex: index,
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          news.articles[index].description!,
-                          style: const TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w400,
+                      );
+                    }),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 15),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Hero(
+                                tag: 'articleImage$index',
+                                child: Image.network(
+                                    news.articles[index].urlToImage!)),
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          Text(
+                            news.articles[index].title!,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 22,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            news.articles[index].description!,
+                            style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              return Container();
-            }
-          },
-        ),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
